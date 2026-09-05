a064528d406ec84bfb6a13f929b789d2f1e16234
Biwash Pradhan <biwashpra3883@gmail.com>
2026-09-06 00:38:43 +0545

    chore: optimize the logos for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.efferd.com",
+        pathname: "/logo/**", // Restricts it to only your logo folder for safety
+      },
+    ],
+  },
 };
 
 export default nextConfig;
